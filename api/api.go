1dbbfe691ee8e6a0d971e346a98384e3faf3c16a
Xiaoyang Tan <tanx@uber.com>
2019-12-01 23:41:16 -0800

    only test import .gen

--- api/api.go
+++ api/api.go
@@ -2,10 +2,10 @@ package api
 
 import (
 	"fmt"
-	c "github.com/xytan0056/depimportgen/.gen/client"
+	// c "github.com/xytan0056/depimportgen/.gen/client"
 )
 
 func API() {
 	fmt.Println("I'm depimportgen " )
-	c.Client("dsf")
+	// c.Client("dsf")
 }
